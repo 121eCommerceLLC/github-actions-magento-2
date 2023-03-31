1d87c42e8fa5b30fe68e969f9dbd4b314f98b5c6
Dmitry Furs <dmitryfurs@gmail.com>
2023-03-31 19:46:46 +0300

    ObjectManager Sniff

---
+++ coding-standard/docker/MEQP2/Sniffs/Classes/ObjectManagerClassSniff.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MEQP2\Sniffs\Classes;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+/**
+ * Class ObjectManagerClassSniff
+ * Detects direct ObjectManager class usage.
+ */
+class ObjectManagerClassSniff implements Sniff
+{
+    /**
+     * Violation severity.
+     *
+     * @var int
+     */
+    private $severity = 8;
+
+    /**
+     * String representation of warning.
+     *
+     * @var string
+     */
+    private $warningMessage = 'The direct use of ObjectManager is discouraged. Inject necessary dependencies via constructor.';
+
+    /**
+     * Warning violation code.
+     *
+     * @var string
+     */
+    private $warningCode = 'ObjectManagerClassFound';
+
+    /**
+     * Restricted classes
+     *
+     * @var array
+     */
+    private $classes = [
+        'ObjectManager',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [
+            T_STRING,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $token = $tokens[$stackPtr]['content'];
+        if (in_array($token, $this->classes)) {
+            $phpcsFile->addError($this->warningMessage, $stackPtr, $this->warningCode, [], $this->severity);
+        }
+    }
+}
--- coding-standard/docker/entrypoint.sh
+++ coding-standard/docker/entrypoint.sh
@@ -8,20 +8,17 @@ echo "::add-matcher::${HOME}/problem-matcher.json"
 cd ${GITHUB_WORKSPACE}
 
 test -z "${PATH_TO_CODE}" && PATH_TO_CODE=$INPUT_PATH_TO_CODE
-test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=$INPUT_PHPCS_STANDARD
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=$INPUT_PHPCS_SEVERITY
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=$INPUT_PHPCS_REPORT
 test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=$INPUT_PHPCS_EXTENSIONS
 
 test -z "${PATH_TO_CODE}" && PATH_TO_CODE=/
-test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=Magento2
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=8
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=checkstyle
 test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=php,phtml,js,html,less
 
 echo "PHPCS path to code: ${PATH_TO_CODE}"
 echo "PHPCS report: ${PHPCS_REPORT}"
-echo "PHPCS standard: ${PHPCS_STANDARD}"
 echo "PHPCS severity: ${PHPCS_SEVERITY}"
 echo "PHPCS extensions: ${PHPCS_EXTENSIONS}"
 
@@ -29,5 +26,5 @@ sh -c "/root/.composer/vendor/bin/phpcs \
   --report=${PHPCS_REPORT} \
   --extensions=${PHPCS_EXTENSIONS} \
   --exclude=Magento2.Annotation.MethodAnnotationStructure \
-  --standard=${PHPCS_STANDARD} ${GITHUB_WORKSPACE}${PATH_TO_CODE} \
+  --standard=/ruleset.xml ${GITHUB_WORKSPACE}${PATH_TO_CODE} \
   -s $*"
