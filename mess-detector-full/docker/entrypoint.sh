fc3ec989cb8992e623fd05656037fab420cf53f2
Dmitry Furs <dmitryfurs@gmail.com>
2023-04-07 16:32:57 +0300

    PHP Mess Detector

---
+++ mess-detector-full/docker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+
+cd ${GITHUB_WORKSPACE}
+
+test -z "${PATH_TO_CODE}" && PATH_TO_CODE=$INPUT_PATH_TO_CODE
+
+test -z "${PATH_TO_CODE}" && PATH_TO_CODE=/
+
+echo "PHPMD path to code: ${PATH_TO_CODE}"
+
+echo "Run \"composer install\""
+sh -c "composer install --ignore-platform-reqs --no-interaction --no-progress"
+
+echo "Run \"./vendor/bin/phpmd ${GITHUB_WORKSPACE}${PATH_TO_CODE} github \
+ ./dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml \
+ --suffixes php,phtml\""
+
+sh -c "./vendor/bin/phpmd ${GITHUB_WORKSPACE}${PATH_TO_CODE} github \
+ ./dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml \
+ --suffixes php,phtml"
